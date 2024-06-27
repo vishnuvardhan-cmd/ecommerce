23b087d7d61d70395867360fb91963421e4f1cbf
vishnu <bvishnu123vardhan@gmail.com>
2024-06-27 14:11:28 +0530

    add: carousel for mens_kurta

--- frontend/fashion-world/src/customer/components/homesectioncarousel/HomeSectionCarousel.jsx
+++ frontend/fashion-world/src/customer/components/homesectioncarousel/HomeSectionCarousel.jsx
@@ -1,7 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
 import HomeSectionCard from "../homesectioncard/HomeSectionCard";
 import AliceCarousel from "react-alice-carousel";
-import { HomeSectionCarouselData } from "./HomeSectionCarouselData";
+import { Mens_Kurta } from "../../../data/mens_kurta";
 import { Button } from "@mui/material";
 import KeyboardArrowLeftIcon from "@mui/icons-material/KeyboardArrowLeft";
 
@@ -10,50 +10,27 @@ function HomeSectionCarousel() {
     0: { items: 1 },
     568: { items: 2 },
     720: { items: 3 },
-    1024: { items: 5.5 },
+    1024: { items: 5},
   };
 
-  const items = HomeSectionCarouselData.map((item) => (
-    <HomeSectionCard label={item.imageUrl} />
+  const items = Mens_Kurta.slice(0, 10).map((item) => (
+    <HomeSectionCard product={item} />
   ));
+
+
+
   return (
-    <div className="border border-black">
+    <div className="border">
       <div className="relative p-5 ">
         <AliceCarousel
           items={items}
           disableButtonsControls
           disableDotsControls
-          infinite
           responsive={responsive}
+          autoPlay
+          autoPlayInterval={1000}
+          infinite
         />
-        <Button
-          className="z-50"
-          variant="contained"
-          sx={{
-            position: "absolute",
-            top: "8rem",
-            right: "0rem",
-            transform: "translateX(50%) rotate(180deg)",
-            bgcolor: "white",
-          }}
-          aira-label="next"
-        >
-          <KeyboardArrowLeftIcon sx={{ color: "black" }} />
-        </Button>
-        <Button
-          className="z-50"
-          variant="contained"
-          sx={{
-            position: "absolute",
-            top: "8rem",
-            left: "0rem",
-            transform:"translate(-50%)",
-            bgcolor: "white",
-          }}
-          aira-label="next"
-        >
-          <KeyboardArrowLeftIcon sx={{ color: "black" }} />
-        </Button>
       </div>
     </div>
   );
--- frontend/fashion-world/src/customer/pages/homepage/HomePage.jsx
+++ frontend/fashion-world/src/customer/pages/homepage/HomePage.jsx
@@ -12,7 +12,7 @@ function HomePage() {
         <HomeSectionCarousel />
         <HomeSectionCarousel />
         <HomeSectionCarousel />
-      </div>
+      </div>s
     </div>
   );
 }
